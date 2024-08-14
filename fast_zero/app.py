95aed023c58977cbc1547baf77639dc531453837
Ivan Noleto <ivannoleto@hotmail.com>
2024-08-14 19:32:20 -0300

    feat: Add endpoint for returning HTML response

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fast_zero.schemas import Message
 
@@ -10,3 +11,16 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/ola', response_class=HTMLResponse)
+def ola():
+    return """
+    <html>
+      <head>
+        <title>Olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo! </h1>
+      </body>
+    </html>"""
