8cbe02d0e4ca8ca823bd93c5138e8db3b5eb9a69
Ivan Noleto <ivannoleto@hotmail.com>
2024-08-16 16:45:44 -0300

    test: Add tests for login with incorrect email and password

--- tests/test_app.py
+++ tests/test_app.py
@@ -164,3 +164,23 @@ def test_get_token(client, user):
     assert response.status_code == HTTPStatus.OK
     assert 'access_token' in token
     assert 'token_type' in token
+
+
+def test_login_for_acess_token_incorrect_email(client):
+    response = client.post(
+        '/token',
+        data={'username': 'test@test.com', 'password': 'test'},
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Incorrect email or password'}
+
+
+def test_login_for_acess_token_incorrect_password(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': 'test'},
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Incorrect email or password'}
