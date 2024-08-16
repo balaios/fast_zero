1393d5593235eb978611729d961a3534b885b2a2
Ivan Noleto <ivannoleto@hotmail.com>
2024-08-16 08:06:01 -0300

    test: Add tests for creating users with existing username and email

--- tests/test_app.py
+++ tests/test_app.py
@@ -45,6 +45,34 @@ def test_create_user(client):
     }
 
 
+def test_create_user_username_already_exists(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Teste',
+            'email': 'teste@test.com',
+            'password': 'testtest',
+        },
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Username already exists'
+    }
+
+
+def test_create_user_email_already_exists(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Teste1',
+            'email': 'teste@test.com',
+            'password': 'testtest',
+        },
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Email already exists'
+    }
+
+
 def test_read_user(client, user):
     response = client.get('/users/1')
     assert response.status_code == HTTPStatus.OK
